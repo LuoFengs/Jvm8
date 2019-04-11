bf2b2db6f04e80cbfcf64d4076b86c89891d3b84
front-luo <front.luo@foxmail.com>
2019-04-11 15:32:38 +0800

    项目初始化

---
+++ src/main/java/com/example/jvm8/Jvm8Application.java
@@ -0,0 +1,13 @@
+package com.example.jvm8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jvm8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jvm8Application.class, args);
+    }
+
+}
